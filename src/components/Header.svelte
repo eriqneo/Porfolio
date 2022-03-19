2bc98996815dfb0a93044a0e3ca412572741e741
eriqneo <eriqneone@gmail.com>
2022-03-19 19:54:03 +0300

    More commit

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <section id="home">
-  <div class="flex flex-wrap py-2 sticky top-0">
+  <div class="flex flex-wrap py-2">
     <div class="w-full px-4 ">
       <nav
         class="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg rounded"
@@ -21,7 +21,7 @@
           >
             <a class="flex flex-row gap-2" href="#home">
               <img {src} alt="logo" class="w-16 rounded-full backdrop-sepia " />
-              <p class="text-2xl pt-7"><strong>EriQ NeO...</strong></p>
+              <p class="text-2xl pt-7 italic">EriQ NeO...</p>
             </a>
             <button
               class="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -32,22 +32,22 @@
             </button>
           </div>
           <div
-            class="lg:flex lg:flex-grow items-center {menuShow
+            class="lg:flex lg:flex-grow items-center self-end {menuShow
               ? 'flex'
               : 'hidden'}"
           >
             <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:text-green-400"
+                  class="px-3 py-2 pl-96 flex items-center self-end text-sm uppercase font-bold leading-snug text-green-400 hover:text-white"
                   href="#about"
                 >
                   About
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item ">
                 <a
-                  class="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:text-green-400"
+                  class="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-green-400 hover:text-white"
                   href="#experience"
                 >
                   Process
@@ -55,7 +55,7 @@
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm uppercase font-bold text-white hover:text-green-400"
+                  class="px-3 py-2 flex items-center text-sm uppercase font-bold text-green-400 hover:text-white"
                   href="#projects"
                 >
                   Projects
@@ -63,7 +63,7 @@
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm uppercase font-bold text-white hover:text-green-400"
+                  class="px-3 py-2 flex items-center text-sm uppercase font-bold text-green-400 hover:text-white"
                   href="#services"
                 >
                   My Services
@@ -71,7 +71,7 @@
               </li>
               <li class="nav-item">
                 <a
-                  class="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-white hover:text-green-400"
+                  class="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-green-400 hover:text-white"
                   href="#contact"
                 >
                   Contact
--- src/components/Services.svelte
+++ src/components/Services.svelte
@@ -1,13 +1,45 @@
+<script>
+  let src = "/Us.png";
+  let tick = "tick.png";
+</script>
+
 <section id="services">
   <div class="ml-24 py-24 pt-0">
     <h3 class="text-3xl font-bold text-gray-200 py-6">
       My Services _________________
     </h3>
     <div>
-      <p class="text-2xl font-bold text-gray-400">
-        Below are the Services I offer;
+      <p class="text-2xl font-bold text-gray-400 text-center">
+        Below are the Services I offer:
       </p>
-      <ul
+    </div>
+    <div class="flex flex-row gap-36">
+      <img {src} alt="us talking" class="w-1/16 lg:w-2/5" />
+      <div class="pt-16">
+        <div class="flex flex-row">
+          <img src={tick} alt="tick" class="w-20" />
+          <p class="text-2xl pl-5 pt-4 text-gray-200 ">
+            Building Cool Websites
+          </p>
+        </div>
+        <div class="flex flex-row pt-4">
+          <img src={tick} alt="tick" class="w-20" />
+          <p class="text-2xl pl-5 pt-4 text-gray-200  ">Coaching Newbies</p>
+        </div>
+        <div class="flex flex-row pt-4">
+          <img src={tick} alt="tick" class="w-20" />
+          <p class="text-2xl pl-5 pt-4 text-gray-200 ">Online Marketing</p>
+        </div>
+        <div class="flex flex-row pt-4">
+          <img src={tick} alt="tick" class="w-20" />
+          <p class="text-2xl pl-5 pt-4 text-gray-200 ">Building API's</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- <ul
         class=" flex flex-row justify-evenly list-none pl-10 text-lg text-green-400 pt-10 "
       >
         <li
@@ -31,6 +63,4 @@
           Digital Marketing
         </li>
       </ul>
-    </div>
-  </div>
-</section>
+      -->
